f5fe1151e8131287ce42672a1026ac8b97d437bd
Caroline Vicente <vicente.caroline@hotmail.com>
2025-03-16 17:40:34 -0300

    criando metodo template

---
+++ Python - Python boas práticas de código com PEP8/pep_8_typing/fila_base.py
@@ -0,0 +1,32 @@
+import abc
+
+class FilaBase(metaclass = abc.ABCMeta):
+    codigo:int = 0
+    fila = []
+    clientes_atendidos = []
+    senha_atual:str = ''
+
+    def reseta_fila(self)->None:
+        if self.codigo >= 200:
+            self.codigo = 0
+        else:
+            self.codigo += 1
+
+    def inseri_cliente(self):
+        self.fila.append(self.senha_atual)
+
+    @abc.abstractmethod
+    def gera_senha_atual(self):
+        ...
+    
+    @abc.abstractmethod
+    def atualiza_fila(self):
+        self.reseta_fila()
+        self.gera_senha_atual()
+        self.inseri_cliente()
+
+    @abc.abstractmethod
+    def chama_cliente(self, caixa : int):
+        cliente_atual:str = self.fila.pop(0)
+        self.clientes_atendidos.append(cliente_atual)
+        return(f'Cliente atual: {cliente_atual}, dirija-se ao caixa {caixa}')
